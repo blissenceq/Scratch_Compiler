1677f7337b1c326b9c2cdbf284af53f1b053f33d
TSarkar99 <tsarkar@ee.iitr.ac.in>
2025-04-21 00:48:59 +0530

    Adding token structure

--- complier.h
+++ complier.h
@@ -2,6 +2,7 @@
 #define SCRATCHCOMPILER_H
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int compile_file(const char* filename, const char* out_filename, int file);
 
@@ -10,6 +11,44 @@ struct compile_process* compile_process_create(const char* filename, const char*
 enum {
     COMPILER_FILE_COMPILED_OK,
     COMPILER_FAILED_WITH_ERRORS
+};
+
+enum {
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE
+};
+
+struct token
+{
+    int type;
+    int flags;
+
+    union
+    {
+        char cval;
+        const char* sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void* any;
+    };
+
+    bool whitespace;
+    const char* between_brackets;
+    
+};
+
+struct pos
+{
+    int line;
+    int column;
+    const char* filename;
 };
 
 struct compile_process
