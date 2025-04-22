8c58c944afe3df9dd957785bec7aae1165c8f898
blissenceq <t.sarkar.cs@gmail.com>
2025-04-22 15:45:35 +0530

    Adding support functions and structs for lexical analysis

--- complier.h
+++ complier.h
@@ -4,9 +4,17 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int compile_file(const char* filename, const char* out_filename, int file);
+struct token;
+struct position;
+struct compile_process;
+struct lex_process;
+struct lex_process_functions;
 
+int compile_file(const char* filename, const char* out_filename, int file);
 struct compile_process* compile_process_create(const char* filename, const char* filename_out, int flags);
+char compile_process_next_char(struct lex_process* lex_process);
+char compile_process_peek_char(struct lex_process* lex_process);
+void compile_process_push_char(struct lex_process* lex_process, char c);
 
 enum {
     COMPILER_FILE_COMPILED_OK,
@@ -23,6 +31,10 @@ enum {
     TOKEN_TYPE_COMMENT,
     TOKEN_TYPE_NEWLINE
 };
+
+typedef char (*LEX_PROCESS_NEXT_CHAR)(struct lex_process* process);
+typedef char (*LEX_PROCESS_PEEK_CHAR)(struct lex_process* process);
+typedef void (*LEX_PROCESS_PUSH_CHAR)(struct lex_process* process, char c);
 
 struct token
 {
@@ -56,6 +68,7 @@ struct compile_process
 {
     //The flags to determine how this file should be compiled
     int flags;
+    struct position pos;
 
     struct compile_process_input_file
     {
@@ -66,6 +79,28 @@ struct compile_process
 
     FILE* ofile;
         
+};
+
+struct lex_process_functions
+{
+    LEX_PROCESS_NEXT_CHAR next_char;
+    LEX_PROCESS_PEEK_CHAR peek_char;
+    LEX_PROCESS_PUSH_CHAR push_char;
+};
+
+
+struct lex_process
+{
+    struct position pos;
+    struct vector* token_vec;
+    struct compile_process* compiler;
+
+    int current_expression_count;
+    struct buffer* parentheses_buffer;
+    struct lex_process_functions* function;
+
+    //This is private data that lexer doesn't understand but the person using the lexer does
+    void* private;
 };
 
 #endif
