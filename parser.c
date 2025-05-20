0a63de0b5091b7d0b49110aea7a846350b3a4091
blissenceq <t.sarkar.cs@gmail.com>
2025-05-20 21:15:31 +0530

    Adding parser support for identifier, string, number

--- parser.c
+++ parser.c
@@ -16,26 +16,72 @@ static void parser_ignore_comment_or_newline(struct token *token)
 static struct token *token_next()
 {
     struct token *next_token = vector_peek_no_increment(current_process->token_vec);
-    parser_ignore_comment_or_new_line(next_token);
+    parser_ignore_comment_or_newline(next_token);
     current_process->pos = next_token->pos;
     parser_last_token = next_token;
     return vector_peek(current_process->token_vec);
 }
 
+static struct token *token_peek_next()
+{
+    struct token *token = vector_peek_no_increment(current_process->token_vec);
+    parser_ignore_comment_or_newline(token);
+    return vector_peek_no_increment(current_process->token_vec);
+}
+
+void parse_single_token_to_node()
+{
+    struct token *token = token_next();
+    struct node *node = NULL;
+
+    switch (token->type)
+    {
+    case TOKEN_TYPE_NUMBER:
+        node = node_create(&(struct node){.type = NODE_TYPE_NUMBER, .llnum = token->llnum});
+        break;
+
+    case TOKEN_TYPE_IDENTIFIER:
+        node = node_create(&(struct node){.type = NODE_TYPE_IDENTIFIER, .cval = token->cval});
+        break;
+
+    case TOKEN_TYPE_STRING:
+        node = node_create(&(struct node){.type = NODE_TYPE_STRING, .sval = token->sval});
+        break;
+
+    default:
+        compiler_error(current_process, "Not a single token that is convertable to a node");
+    }
+}
+
 int parse_next()
 {
+    struct token *token = token_peek_next();
+    if (!token)
+        return -1;
+
+    switch (token->type)
+    {
+    case TOKEN_TYPE_NUMBER:
+    case TOKEN_TYPE_IDENTIFIER:
+    case TOKEN_TYPE_STRING:
+        parse_single_token_to_node();
+        break;
+    }
+
     return 0;
 }
 
 int parse(struct compile_process *process)
 {
     current_process = process;
+    parser_last_token = NULL;
+    node_set_vector(current_process->node_vec, current_process->node_tree_vec);
     struct node *node = NULL;
     vector_set_peek_pointer(process->token_vec, 0);
 
     while (parse_next() == 0)
     {
-        node = peek_node();
+        node = node_peek();
         vector_push(process->node_tree_vec, &node);
     }
 
